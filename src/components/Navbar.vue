a66d69702cac7d0e7c4c57a7b3dade7716c9e1b0
HermesSantos <hermessantos995@gmail.com>
2025-09-15 00:43:09 -0300

    Add logo to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,10 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-body-light">
-    <div class="container">
-      <a class="navbar-brand" href="/">House of Wolves</a>
+    <div class="container d-flex align-items-center">
+      <a class="navbar-brand d-flex align-items-center" href="/">
+        <img src="/images/logo.png" alt="Logo" style="width: 40px; height: 40px; object-fit: cover; margin-right: 10px;">
+        <span>House of Wolves</span>
+      </a>
     </div>
   </nav>
 </template>
