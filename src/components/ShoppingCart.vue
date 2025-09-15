60a9e1ddfccdf3d3d6fdb227beb48fcc717f7729
HermesSantos <hermessantos995@gmail.com>
2025-09-15 02:20:35 -0300

    feat: Add shopping cart link to navbar and route

---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue"
+import { useRoute } from "vue-router"
+import apiService from "@/services/apiService"
+
+const route = useRoute()
+
+onMounted(async () => {
+  const response = await apiService.get(`/get-product/${route.params.id}`)
+  product.value = response.data
+})
+const product = ref<any>({
+  price: 0
+})
+
+</script>
+
+<template>
+  <div class="container py-5 d-flex" v-if="product">
+    <div>1</div>
+    <div>2</div>
+  </div>
+</template>
+<style scoped>
+*{
+  color: white
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from '../components/Dashboard.vue'
+import ShoppingCart from '../components/ShoppingCart.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: Dashboard, name: 'Dashboard' },
+    { path: '/product/:id', component: ShoppingCart, name: 'ShoppingCart' },
   ],
 })
 
