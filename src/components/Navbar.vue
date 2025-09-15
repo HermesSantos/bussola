4bd7d681f80e6b004afa170625f55c1fbd964883
HermesSantos <hermessantos995@gmail.com>
2025-09-14 22:03:33 -0300

    feat Add bootstrap and basic layout

---
+++ src/components/Navbar.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-light">
+    <div class="container">
+      <a class="navbar-brand" href="/">House of Wolves</a>
+    </div>
+  </nav>
+</template>
+<style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Dashboard from '../components/Dashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/', component: Dashboard, name: 'Dashboard' },
+  ],
 })
 
 export default router
