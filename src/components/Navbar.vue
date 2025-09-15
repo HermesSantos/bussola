011be5ea7715a6f4c02ac55644ee830cf311a4d9
HermesSantos <hermessantos995@gmail.com>
2025-09-15 00:55:02 -0300

    Update logo navbar and styles for better responsiveness

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,27 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-body-light">
+  <nav class="navbar navbar-expand-lg bg-body-light fixed-top">
     <div class="container d-flex align-items-center">
       <a class="navbar-brand d-flex align-items-center" href="/">
         <img src="/images/logo.png" alt="Logo" style="width: 40px; height: 40px; object-fit: cover; margin-right: 10px;">
-        <span>House of Wolves</span>
+        <span class="hover-effect">House of Wolves</span>
       </a>
     </div>
   </nav>
 </template>
 <style scoped>
-a {
+span {
   color: white
 }
 .navbar {
   background-color: #ffffff4d;
   backdrop-filter: blur(10px);
   padding: 20px;
+}
+.hover-effect {
+  transition: color 0.3s ease;
+}
+
+.hover-effect:hover {
+  color: #a1a1a1;
 }
 </style>
