4dd1c640bb90975a29756bbe09c4bdc1d36cab2a
HermesSantos <hermessantos995@gmail.com>
2025-09-14 23:03:10 -0300

    Feat Add Axios and update API URL improve dashboard component

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
 import Card from "./Card.vue";
+import { ref, onMounted } from 'vue'
+import apiService from '../services/apiService'
+
+const message = ref('Olá!')
+
+async function fetchProducts () {
+  const response = await apiService.get('/')
+  console.log(response.data)
+}
+onMounted(() => {
+  fetchProducts()
+})
 </script>
 <template>
   <div class="container pt-5">
